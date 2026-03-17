4937d5b87fb234affe030b58ea8287b3aba58a23
ArtVel-34 <avelik1997@gmail.com>
2026-03-17 12:23:41 +0100

    added sum and mult functions

--- main.py
+++ main.py
@@ -1,2 +1,7 @@
-print("Nie mówię po polsku.")
-#rcftegwyio
+def sum_of_2_nums(a, b):
+    return a + b
+
+
+a = int(input("Enter a number: "))
+b = int(input("Enter another number: "))
+print(f"Sum of {a} and {b} is:356", sum_of_2_nums(a, b))
--- second_file.py
+++ second_file.py
@@ -1,2 +1,7 @@
+def mult_of_2_nums(a, b):
+    return a * b
 
-print("No hablo espanol.")
+
+a = int(input("Enter a number: "))
+b = int(input("Enter another number: "))
+print(f"The product of {a} and {b} is:", mult_of_2_nums(a, b))
