468cc061e2dd5de863e5effeedbaed8ae32e2f3f
ArtVel-34 <avelik1997@gmail.com>
2026-03-17 12:36:20 +0100

    Revert "added divison function to third_file.py"
    This reverts commit a4ba99f040caf5775e704ac6ff1c1a0f4accf1b0.

--- third_file.py
+++ third_file.py
@@ -1,7 +1,3 @@
-def div_of_2_nums(a, b):
-  return a/b
-
-
+print("Ich spreche kein Deutsch.")
 a = int(input("Enter a number: "))
-b = int(input("Enter another number: "))
-print(f"Division of {a} by {b} is:", div_of_2_nums(a, b))
+print("Your number is:", a)
