a4ba99f040caf5775e704ac6ff1c1a0f4accf1b0
ArtVel-34 <avelik1997@gmail.com>
2026-03-17 12:22:34 +0100

    added divison function to third_file.py
    It will break if you assign b to 0

--- third_file.py
+++ third_file.py
@@ -1,3 +1,7 @@
-print("Ich spreche kein Deutsch.")
+def div_of_2_nums(a, b):
+  return a/b
+
+
 a = int(input("Enter a number: "))
-print("Your number is:", a)
+b = int(input("Enter another number: "))
+print(f"Division of {a} by {b} is:", div_of_2_nums(a, b))
